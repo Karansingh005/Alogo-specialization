46945ccc2dd9f99f929426f2ce7692b5f8352847
Karan Singh <karansinghreen@gmail.com>
2020-07-07 15:17:33 +0530

    Update sum.java
    Changed classname

--- Algorithm Toolbox/sum.java
+++ Algorithm Toolbox/sum.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.lang.*;
 
-class abc
+class sum
 {
     public static int sum(int a, int b){
         return a+b;
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
